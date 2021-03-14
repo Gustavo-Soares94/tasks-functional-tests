089112cd46443ceb7564c673572be0d22843db67
Gustavo Soares <gustavosoares.adm@gmail.com>
2021-03-14 20:05:38 -0300

    initial version

---
+++ src/test/java/testsFuncional/TasksTest.java
@@ -0,0 +1,43 @@
+package testsFuncional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class TasksTest {
+
+    public WebDriver acessarAplicacao(){
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8001/tasks");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso(){
+        WebDriver driver = acessarAplicacao();
+
+        //Clicar em Add
+        driver.findElement(By.id("addTodo")).click();
+
+        //escrever descricao
+        driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+
+        //escrever a data
+        driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+
+        //clicar em salvar
+        driver.findElement(By.id("saveButton")).click();
+
+        //validar mensagem de sucesso
+        String message = driver.findElement(By.id("message")).getText();
+        Assert.assertEquals("Sucess!", message);
+
+        //fechar o browser
+        driver.quit();
+    }
+
+}
